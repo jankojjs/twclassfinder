dfbc7115f33b43cfa5f4ca71f6b87e0c89911744
Janko Stanic <janko.stanic@activecollab.com>
2024-06-01 10:56:58 +0200

    Add check for prefix

--- extension.js
+++ extension.js
@@ -1,5 +1,7 @@
 const vscode = require("vscode");
 const styles = require("./styles");
+const fs = require("fs");
+const path = require("path");
 
 /**
  * @param {vscode.ExtensionContext} context
@@ -31,11 +33,35 @@ function activate(context) {
         Object.keys(styles).map((item, index) => {
           if (classValue.includes(`tw:${item}`)) {
             Object.values(styles)[index].map((style) => {
+              // Get Tailwind prefix if tailwind.config.js file exists
+              let tailwindPrefix = ""; // Default prefix
+              const workspaceFolders = vscode.workspace.workspaceFolders;
+              if (workspaceFolders && workspaceFolders.length > 0) {
+                const workspacePath = workspaceFolders[0].uri.fsPath;
+                const tailwindConfigPath = path.join(
+                  workspacePath,
+                  "tailwind.config.js"
+                );
+
+                if (fs.existsSync(tailwindConfigPath)) {
+                  const configFile = fs.readFileSync(
+                    tailwindConfigPath,
+                    "utf-8"
+                  );
+                  const prefixMatch = configFile.match(
+                    /prefix:\s*['"](.+?)['"]/
+                  );
+                  if (prefixMatch) {
+                    tailwindPrefix = prefixMatch[1];
+                  }
+                }
+              }
               let completionItem = new vscode.CompletionItem(
                 style.value,
                 vscode.CompletionItemKind.Value
               );
               completionItem.detail = style.desc;
+              completionItem.insertText = `${tailwindPrefix}${style.value}`;
               const rangeToRemove = new vscode.Range(
                 position.line,
                 lineText.indexOf(`tw:${item}`),
